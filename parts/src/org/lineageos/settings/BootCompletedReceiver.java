b81fc034581aa69f92996411af34c0120767eb46
Lostark13 <tushar9915@gmail.com>
2023-04-10 19:16:45 +0300

    surya: parts: Add per app maximum refresh rate
    Signed-off-by: Lostark13 <tushar9915@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -26,6 +26,7 @@ import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.haptic.HapticUtils;
+import org.lineageos.settings.refreshrate.RefreshUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
@@ -49,5 +50,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
         // Haptic
         HapticUtils.restoreLevel(context);
+        RefreshUtils.startService(context);
     }
 }
