a6f1768133cac3c2484c55b9b7dccda36d9ce64c
rxuglr <rxuglr@mail.ru>
2023-04-26 19:44:43 +0300

    surya: parts: Remove dirac

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,7 +23,6 @@ import android.content.Intent;
 import android.util.Log;
 
 import org.lineageos.settings.doze.DozeUtils;
-import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.haptic.HapticUtils;
@@ -39,11 +38,9 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         }
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
-	    // Doze
-        DozeUtils.onBootCompleted(context);
 
-        // Dirac
-        DiracUtils.initialize(context);
+        // Doze
+        DozeUtils.onBootCompleted(context);
 
         // Refresh Rate
         RefreshUtils.startService(context);
